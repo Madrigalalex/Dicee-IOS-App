a82217d4ffa86f3123c7d491e688684f9b2fa3c8
Madrigalalex88 <Madrigalalex88@yahoo.com>
2019-05-05 22:56:35 -0700

    Initial Commit

---
+++ DIcee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DIcee
+//
+//  Created by Alex Madrigal on 5/5/19.
+//  Copyright © 2019 Alex Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
